1d5b88b9eb4b0d3bf442e7b30c0a174bf97a9ea7
EC2 Default User <ec2-user@ip-172-31-45-74.us-east-2.compute.internal>
2021-11-14 16:29:10 +0000

    init

---
+++ my-app/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
+    { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
+    { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
+    { path: '/notifications', title: 'Notifications',  icon:'ui-1_bell-53', class: '' },
+
+    { path: '/user-profile', title: 'User Profile',  icon:'users_single-02', class: '' },
+    { path: '/table-list', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
+    { path: '/typography', title: 'Typography',  icon:'text_caps-small', class: '' },
+    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'objects_spaceship', class: 'active active-pro' }
+
+];
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { 
+    this.menuItems=[]
+  }
+
+  ngOnInit(): void {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  
+  isMobileMenu() {
+      if ( window.innerWidth > 991) {
+          return false;
+      }
+      return true;
+  };
+
+}
